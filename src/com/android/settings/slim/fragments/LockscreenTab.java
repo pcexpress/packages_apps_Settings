3a97a8fbb2da19c1718d94aba7bf81797baa1583
0xD34D <clark@scheffsblend.com>
2013-09-15 04:18:00 +0800

    Add active display to Carbon [2/2]
    PS2:
    Add pocket mode option, available if the device has
    a proximity sensor.
    PS3:
    Add option to include ongoing and non-clearable notifications.
    PS4:
    Allow brightness control from 0% to 100%
    PS5:
    Refactor for release
    
    Change-Id: Iae55640a31df68542c2af5d95097811b8fb1f66f

---
+++ src/com/android/settings/slim/fragments/LockscreenTab.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2012-2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.slim.fragments;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Resources;
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.PreferenceDrawerActivityAlt;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import com.android.settings.slim.fragments.*;
+
+import java.util.ArrayList;
+
+public class LockscreenTab extends SettingsPreferenceFragment {
+
+    private static final String TAG = "Lockscreen_Category";
+
+    PagerTabStrip mPagerTabStrip;
+    ViewPager mViewPager;
+
+    ViewGroup mContainer;
+
+    String titleString[];
+
+    static Bundle mSavedState;
+
+    public boolean hasButtons() {
+        return !getResources().getBoolean(com.android.internal.R.bool.config_showNavigationBar);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        mContainer = container;
+
+        View view = inflater.inflate(R.layout.pager_tab, container, false);
+        mViewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        mPagerTabStrip = (PagerTabStrip) view.findViewById(R.id.pagerTabStrip);
+
+        LsAdapter LsAdapter = new LsAdapter(getFragmentManager());
+        mViewPager.setAdapter(LsAdapter);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (!Utils.isTablet(getActivity())) {
+            mContainer.setPadding(0, 0, 0, 0);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    class LsAdapter extends FragmentPagerAdapter {
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        public LsAdapter(FragmentManager fm) {
+            super(fm);
+            if (!hasButtons()) {
+                frags[0] = new LockscreenGeneral();
+                frags[1] = new LockscreenAdvanced();
+                frags[2] = new LockscreenTargets();
+                frags[3] = new ActiveDisplay();
+            } else {
+                frags[0] = new LockscreenGeneral();
+                frags[1] = new LockscreenAdvanced();
+                frags[2] = new LockscreenTargets();
+                frags[3] = new ActiveDisplay();
+                frags[4] = new LockscreenButtons();
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+    }
+
+    private String[] getTitles() {
+        if (!hasButtons()) {
+            titleString = new String[]{
+                    getString(R.string.lock_screen_general_category),
+                    getString(R.string.lock_screen_advanced_category),
+                    getString(R.string.lock_screen_targets_category),
+                    getString(R.string.lock_screen_ad_category)};
+        } else {
+            titleString = new String[]{
+                    getString(R.string.lock_screen_general_category),
+                    getString(R.string.lock_screen_advanced_category),
+                    getString(R.string.lock_screen_targets_category),
+                    getString(R.string.lock_screen_ad_category),
+                    getString(R.string.lock_screen_buttons_category)};
+
+        }
+        return titleString;
+    }
+}
