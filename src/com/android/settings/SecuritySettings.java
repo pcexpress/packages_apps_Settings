3495fac17efd6678fd1bab57bb03152a9a54d039
pcexpress <pcexpress805@gmail.com>
2013-10-19 11:32:00 -0700

    Revert "Revert "[2/2] Settings: Add lockscreen ring battery setting""
    This reverts commit 64f15aa8311681ff77a56ee4efc17962d23ec249.

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -106,6 +106,9 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String KEY_BLACKLIST = "blacklist";
     private static final String PREF_ADVANCED_REBOOT_KEY = "advanced_reboot";
 
+    // Omni Additions
+    private static final String BATTERY_AROUND_LOCKSCREEN_RING = "battery_around_lockscreen_ring";
+
     private PackageManager mPM;
     DevicePolicyManager mDPM;
 
@@ -145,6 +148,9 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private ListPreference mSmsSecurityCheck;
     private PreferenceScreen mBlacklist;
     private ListPreference mAdvancedReboot;
+
+    // Omni Additions
+    private CheckBoxPreference mLockRingBattery;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -323,6 +329,12 @@ public class SecuritySettings extends SettingsPreferenceFragment
             mVibratePref = (CheckBoxPreference) findPreference(KEY_VIBRATE_PREF);
             mVibratePref.setChecked(Settings.System.getInt(resolver,
                     Settings.System.LOCKSCREEN_VIBRATE_ENABLED, 1) == 1);
+
+            // Add the additional Omni settings
+            mLockRingBattery = (CheckBoxPreference) root
+                    .findPreference(BATTERY_AROUND_LOCKSCREEN_RING);
+            mLockRingBattery.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.BATTERY_AROUND_LOCKSCREEN_RING, 0) == 1);
 
             // disable lock options if lock screen set to NONE
             // or if using pattern as a primary lock screen or
@@ -797,6 +809,9 @@ public class SecuritySettings extends SettingsPreferenceFragment
         } else if (preference == mVibratePref) {
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.LOCKSCREEN_VIBRATE_ENABLED, isToggled(preference) ? 1 : 0);
+        } else if (preference == mLockRingBattery) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.BATTERY_AROUND_LOCKSCREEN_RING, isToggled(preference) ? 1 : 0);
         } else if (preference == mShowPassword) {
             Settings.System.putInt(getContentResolver(), Settings.System.TEXT_SHOW_PASSWORD,
                     mShowPassword.isChecked() ? 1 : 0);
